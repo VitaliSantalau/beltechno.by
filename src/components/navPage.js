6cf5b9e5518380e8b2e288a565421e1760448e60
VitaliSantalau <v.santalau@gmail.com>
2021-02-17 18:40:15 +0300

    created mobile nav oage

--- src/components/navPage.js
+++ src/components/navPage.js
@@ -1,11 +1,25 @@
-import * as React from "react"
+import style from "./navPage.module.css"
+import React, { useState } from "react"
 import { Link } from "gatsby"
-import style from "./navPage.module.css";
+
 
 
 export default function NavPage() {
-  
+  const [isActiveIconNavPage, setIsActiveIconNavPage] = useState(false)
+  const styleIconNavPage =  isActiveIconNavPage ?
+    style.iconNavPageActive : style.iconNavPage
+    
+  const styleOverlayNavPage =  isActiveIconNavPage ?
+    style.overlayActive : style.overlay
+
+  const handleClickIconNavPage = () => {
+    isActiveIconNavPage ?
+    setIsActiveIconNavPage(false) :
+    setIsActiveIconNavPage(true)
+  }
+
   return (
+    <>
     <nav className={style.nav}>
       <Link to="/">
         Главная
@@ -20,5 +34,43 @@ export default function NavPage() {
         Контакты
       </Link>
     </nav>
+    <div className={styleIconNavPage} onClick={handleClickIconNavPage}></div>
+    <div className={styleOverlayNavPage}>
+      <nav className={style.navMobile}>
+        <Link to="/">
+          <div onClick={handleClickIconNavPage}>Главная</div>
+        </Link>
+        <Link to="/about">
+          О компании
+        </Link>
+        <Link to="/catalog">
+          Каталог
+        </Link>
+        <Link to="/contacts">
+          Контакты
+        </Link>
+        <Link to="/contacts">
+          <div className={style.navSection}>
+            Бумага
+          </div>
+        </Link>
+        <Link to="/contacts">
+        <div className={style.navSection}>
+          Автомобили
+        </div>
+        </Link>
+        <Link to="/contacts">
+        <div className={style.navSection}>
+          Посуда
+        </div>
+        </Link>
+        <Link to="/contacts">
+        <div className={style.navSection}>
+          Овощи
+        </div>  
+        </Link>
+      </nav>
+    </div>
+    </>
   )
-};
+}
--- src/components/navSection.js
+++ src/components/navSection.js
@@ -7,18 +7,22 @@ export default function NavSection() {
   
   return (
     <nav className={style.nav}>
+      <div className={style.row}>
       <Link to="/">
         <div className={style.rolls}></div>
       </Link>
       <Link to="/">
         <div className={style.cars}></div>
       </Link>
+      </div>
+      <div className={style.row}>
       <Link to="/">
         <div className={style.cups}></div>
       </Link>
       <Link to="/">
         <div className={style.vegetables}></div>
       </Link>
+      </div>      
     </nav>
   )
 }
