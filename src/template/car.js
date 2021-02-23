ceb689c61295224dc20dffa18c7e6ab844725735
VitaliSantalau <v.santalau@gmail.com>
2021-02-24 00:26:03 +0300

    next

--- src/template/car.js
+++ src/template/car.js
@@ -4,12 +4,8 @@ import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
 import Header from "../components/header"
 import Footer from "../components/footer"
-import SEO from "../components/SEO"
-import styled from '@emotion/styled'
-import { css, jsx } from '@emotion/react'
-import Slider from "react-slick"  
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import SEO from "../components/SEO" 
+import Carousel from "../components/carousel"
 
 
 export const query = graphql`
@@ -55,7 +51,6 @@ const ItemCard = ({ data }) => {
   const item = data.contentfulCar
 
   
-  
   return (
     <>
       <SEO 
@@ -67,7 +62,10 @@ const ItemCard = ({ data }) => {
         <Header />
         <main className={style.main}>
           <div className={style.wrapper}>
-<AsNavFor />
+            <div className={style.containerCarousel}>
+              <Carousel />
+            </div>
+            
           </div>
         </main>
         <Footer />
@@ -75,83 +73,5 @@ const ItemCard = ({ data }) => {
     </>
   )
 }
+
 export default ItemCard;
-
-export class AsNavFor extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      nav1: null,
-      nav2: null
-    };
-  }
-
-  componentDidMount() {
-    this.setState({
-      nav1: this.slider1,
-      nav2: this.slider2
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>Slider Syncing (AsNavFor)</h2>
-        <h4>First Slider</h4>
-        <Slider
-          asNavFor={this.state.nav2}
-          ref={slider => (this.slider1 = slider)}
-          arrows={false}
-          className={style.slider}
-        >
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>
-        <h4>Second Slider</h4>
-        <Slider
-          asNavFor={this.state.nav1}
-          ref={slider => (this.slider2 = slider)}
-          slidesToShow={5}
-          swipeToSlide={true}
-          focusOnSelect={true}
-          className={style.navSlider}
-        >
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>
-      </div>
-    );
-  }
-}
