93740a94d55e838414e3ee0c9c02cfcaff2f0a5f
VitaliSantalau <v.santalau@gmail.com>
2021-03-19 00:29:16 +0300

    fix video

--- src/components/navSection.js
+++ src/components/navSection.js
@@ -8,20 +8,20 @@ export default function NavSection() {
   return (
     <nav className={style.nav}>
       <div className={style.row}>
-      <Link to="/rolls/">
-        <div className={style.rolls}></div>
-      </Link>
-      <Link to="/cars/">
-        <div className={style.cars}></div>
-      </Link>
+        <Link to="/rolls/">
+          <div className={style.rolls}></div>
+        </Link>
+        <Link to="/cars/">
+          <div className={style.cars}></div>
+        </Link>
       </div>
       <div className={style.row}>
-      <Link to="/cups/">
-        <div className={style.cups}></div>
-      </Link>
-      <Link to="/vegetables/">
-        <div className={style.vegetables}></div>
-      </Link>
+        <Link to="/cups/">
+          <div className={style.cups}></div>
+        </Link>
+        <Link to="/vegetables/">
+          <div className={style.vegetables}></div>
+        </Link>
       </div>      
     </nav>
   )
